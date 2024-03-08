9c7ebe02fb85347ae74e88e967c74f201deb25e8
C_Vtd <cloe.vautard@gmail.com>
2024-03-08 19:57:04 +0100

    Update

--- ProcessCommunication.c
+++ ProcessCommunication.c
@@ -5,9 +5,10 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int nb_pipes;
-int **pipe_fds;
-pid_t *child_pids;
+int nb_pipes = 0;
+int nb_processes = 0;
+int **pipe_fds = NULL;
+pid_t *child_pids = NULL;
 
 void init_pipes(int n) {
   nb_pipes = (1 << n) * n;
@@ -19,7 +20,7 @@ void init_pipes(int n) {
   }
 
   for (int i = 0; i < nb_pipes; i++) {
-    pipe_fds[i] = (int) malloc(2 * sizeof(int));
+    pipe_fds[i] = (int *) malloc(2 * sizeof(int));
     if(pipe_fds[i] == NULL)
     {
       perror("Pipe allocation failed 2");
@@ -33,13 +34,21 @@ void init_pipes(int n) {
 }
 
 void create_hypercube_processes(int n) {
-  int nb_processes = 1 << n;
+  nb_processes = 1 << n;
+
+  child_pids = (pid_t *) malloc(nb_processes * sizeof(pid_t));
+  if(child_pids == NULL)
+  {
+    perror("child_pids allocation failed 2");
+    exit(EXIT_FAILURE);
+  }
+
   for (int i = 0; i < nb_processes; i++) {
     pid_t pid = fork();
     if (pid == -1) {
       perror("Failed to fork");
       exit(EXIT_FAILURE);
-    } else if (pid == 0) {
+    } else if (pid == 0) { // childs
       for (int j = 0; j < nb_processes; j++) {
         for (int k = 0; k < 2; k++) {
           if (j != i) {
@@ -61,31 +70,34 @@ void create_hypercube_processes(int n) {
         }
       }
       exit(0);
-    } else {
+    } else { // father
       child_pids[i] = pid;
     }
   }
-  for (int i = 0; i < nb_processes; i++) {
+
+  
+  for (int i = 0; i < nb_pipes; i++) {
     close(pipe_fds[i][0]);
     close(pipe_fds[i][1]);
   }
 }
 
 void wait_for_children(int n) {
-  int nb_processes = 1 << n;
   for (int i = 0; i < nb_processes; i++) {
     int status;
     waitpid(child_pids[i], &status, 0);
   }
 }
 
-
 void free_pipes()
 {
   for(int i = 0; i < nb_pipes; i++)
   {
     free(pipe_fds[i]);
+    pipe_fds[i] = NULL;
   }
+  free(pipe_fds);
+  pipe_fds = NULL;
 }
 
 void free_child_pids()
--- Tests/test1.c
+++ Tests/test1.c
@@ -5,6 +5,25 @@
 #include <sys/select.h>
 
 
+/*
+    |----< INFOS >----|
+
+-> Pour un hypercude de dimension n:
+    - 2 puissance n sommets
+    - (2 puissance n-1) * n arrêtes
+    - (2 * (2 puissance n-1) * n) == ((2 puissance n) * n) tubes
+
+-> (1 << n) == (2 puissance n)
+
+-> Opérateurs binaires : 
+    - i & j : ET
+    - i ^ j : OU EXCLUSIF (mets les bits différents à 1, les autres à 0)
+    - i | j : OU INCLUSIF
+
+    - Dans notre cas, comme deux id de sommets adjacents on un seul bit de différent, 
+      le ET binaire donne le min des deux id et le OU INCLUSIF donne le max des deux id
+*/
+
 int difference1bit(int a, int b, int *bitDiffPos);
 
 int main()
@@ -21,11 +40,15 @@ int main()
         {
             if(difference1bit(i, j, &bitDiffPos)) // si les 2 id ont 1 seul bit de différence
             {
-                // celui avec l'id le plus grand lit dans i & j et écrit dans id_processus i | j
+                // celui avec l'id le plus grand lit dans i | j et écrit dans id_processus i & j
                 // ici, on a toujours j > i
 
-                printf("%d --> %d : %d - %d\n", i, j, j & i, bitDiffPos);
-                printf("%d <-- %d : %d - %d\n", i, j, j | i, bitDiffPos);
+                // printf("%d --> %d : %d - %d\n", i, j, j | i, bitDiffPos);
+                // printf("%d <-- %d : %d - %d\n", i, j, j & i, bitDiffPos);
+                
+                printf("%d --> %d : %d - %d\n", i, j, j | i, bitDiffPos);
+                printf("%d <-- %d : %d - %d\n", i, j, j & i, bitDiffPos);
+                
                 // id1 <--> id2 : [...][...]
                 // (lire) <-- (écrire)
             }
